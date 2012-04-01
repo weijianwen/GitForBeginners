74eeb0b32ba4deb1badb70577c8ea64b39253dbd
Jianwen Wei <wei.jianwen@gmail.com>
2012-04-01 19:38:37 +0800

    Restart the git-tutorial project.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-pandoc gittutorial.md --standalone -f markdown -t beamer -o git_beamer.tex -V author="Jianwen WEI" -V fontsize=14pt -V theme=Warsaw -V title="Where's My Water?" -V date="\today"
+pandoc gittutorial.mkd --standalone -f markdown -t beamer -o git_beamer.tex -V author='Jianwen WEI' -V fontsize=11pt -V theme=Warsaw -V title='Learning by Doing\\{}A Short Introduction to git' -V date='\today'
 
