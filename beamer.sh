f3923ec0529d29a610136446c9281c4df84ea08a
Jianwen Wei <wei.jianwen@gmail.com>
2012-04-05 18:13:29 +0800

    Rearrange the list: - The first line is the command, such as git clone - The second line has a extra indent, nested in the upper layer,   contains the description. An example is "Clone the project."

--- beamer.sh
+++ beamer.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 TEXFILE="git_beamer_base.tex"
+BEAMERFILE="git_beamer.tex"
 PDFFILE="git_beamer_base.pdf"
 
 pandoc gittutorial.mkd --standalone -f markdown -t beamer -o $TEXFILE -V author='Jianwen WEI' -V fontsize=11pt -V theme=Boadilla -V title='Learning by Doing\\{}A Short Introduction to git' -V date='\today'
 
-latexmk -pv -pdf $TEXFILE 
+# latexmk -pv -pdf $TEXFILE 
+
+diff $TEXFILE $BEAMERFILE 
 
 
